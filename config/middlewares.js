d502397583f95ca8c2cfe20d3f8c1ee7f8300333
Krishna Nagpure <krishnagnagpure@gmail.com>
2023-12-14 16:15:45 +0530

    changes on home page

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,7 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://3.110.97.65:3000', 'http://15.207.20.62:1337']
-    },
-  },
+  'strapi::cors',
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
