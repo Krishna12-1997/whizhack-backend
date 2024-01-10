e824501efbcb51a30b66439fe013093d3b4c889a
Krishna Nagpure <krishnagnagpure@gmail.com>
2024-01-10 16:51:10 +0530

    Add Slider image on home

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,11 +2,19 @@ module.exports = [
   "strapi::errors",
   "strapi::security",
   {
-    name: 'strapi::cors',
+    name: "strapi::cors",
     config: {
-      origin: ['http://13.200.174.138:80', 'http://15.207.226.239:1337']
+      origin: [
+        "http://13.200.174.138:80",
+        "http://13.200.174.138:3000",
+        "http://15.207.226.239:1337",
+        "http://whizhack.com",
+        "http://www.whizhack.com",
+        "http://test.whizhack.com",
+      ],
     },
   },
+
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
