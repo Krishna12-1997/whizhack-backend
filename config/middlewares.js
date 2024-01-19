36982409ddee71b5f3c40156762ce4c8ea5fac09
Krishna <krishnagnagpure@gmail.com>
2024-01-19 12:37:56 +0530

    add s3 bucket for image

--- config/middlewares.js
+++ config/middlewares.js
@@ -14,7 +14,6 @@ module.exports = [
         "http://www.whizhack.com",
         "https://test.whizhack.com",
         "http://test.whizhack.com",
-        "https://d2vyuniai6aaiz.cloudfront.net"
       ],
     },
   },
