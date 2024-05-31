fbdf47930f18965749e014c0e67de528e455d232
Krishna <krishnagnagpure@gmail.com>
2024-05-31 19:07:50 +0530

    Add team member

--- src/admin/app.example.js
+++ src/admin/app.example.js
@@ -1,33 +1,27 @@
+
 const config = {
-  locales: [
-    // 'ar',
-    // 'fr',
-    // 'cs',
-    // 'de',
-    // 'dk',
-    // 'es',
-    // 'he',
-    // 'id',
-    // 'it',
-    // 'ja',
-    // 'ko',
-    // 'ms',
-    // 'nl',
-    // 'no',
-    // 'pl',
-    // 'pt-BR',
-    // 'pt',
-    // 'ru',
-    // 'sk',
-    // 'sv',
-    // 'th',
-    // 'tr',
-    // 'uk',
-    // 'vi',
-    // 'zh-Hans',
-    // 'zh',
-  ],
+  auth: {
+    logo: "../extensions/Registered WhizHack Logo 1.webp",
+  },
+  // Replace the favicon
+  head: {
+    favicon: "../extensions/Registered WhizHack Logo 1.webp",
+  },
+  menu: {
+    logo: "../extensions/Registered WhizHack Logo 1.webp",
+  },
+  theme: {
+    colors: {
+      primary100: '#F8F7F4',
+      primary200: '#F2EFE9',
+      primary500: '#262730',
+      primary600: '#BF2F28',
+      primary700: '#D84942',
+      danger700: '#BF2F28'
+    }
+  }
 };
+
 
 const bootstrap = (app) => {
   console.log(app);
